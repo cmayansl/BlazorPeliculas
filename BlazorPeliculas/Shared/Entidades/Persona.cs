851a42559a8d054381e4d431624c47dcb39a16c7
cristina <cristina.mayans@gmail.com>
2020-04-01 14:00:10 +0200

    Sección 5 - Formulario: Formulario de editar/crear Personas

---
+++ BlazorPeliculas/Shared/Entidades/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorPeliculas.Shared.Entidades
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Nombre { get; set; }
+        public string Biografia { get; set; }
+        public string Foto { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+    }
+}
